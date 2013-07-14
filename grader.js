3ff0ca6ab7bfd4f445950d7362a859d46089a845
Andrew Krause <a.c.krause@gmail.com>
2013-07-14 20:57:18 +0000

    Added ability to check files via URL

--- grader.js
+++ grader.js
@@ -24,8 +24,10 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -36,13 +38,21 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
+
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
 
+
+var cheerioRawHtml = function(html) {
+    return cheerio.load(html);
+};
+
+
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
-}
+};
+
 
 var checkHtmlFile = function(htmlfile, checksfile) {
     $ = cheerioHtmlFile(htmlfile);
@@ -55,20 +65,49 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+
+var buildUrlChecker = function(checksfile) {
+    var checkUrl = function (result, response) {
+      if (result instanceof Error) {
+        console.error('Unable to read URL');
+        return {};
+      } else {
+        $ = cheerioRawHtml(result);
+        var checks = loadChecks(checksfile).sort();
+        var out = {};
+        for (var ii in checks) {
+            var present = $(checks[ii]).length > 0;
+            out[checks[ii]] = present;
+        }
+        outJson = JSON.stringify(out, null, 4);
+        console.log(outJson);
+      }
+    }
+    return checkUrl;
+};      
+    
+
 var clone = function(fn) {
     // Workaround for commander.js issue
     // http://stackoverflow.com/a/6772648
     return fn.bind({});
 };
 
+
 if(require.main == module) {
     program
       .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
       .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+      .option('-u, --url <url>', 'URL of file to check', '')
       .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if (program.url) {
+      var checkUrl = buildUrlChecker(program.checks);
+      rest.get(program.url).on('complete', checkUrl);
+    } else {
+      var checkJson = checkHtmlFile(program.file, program.checks);
+      var outJson = JSON.stringify(checkJson, null, 4);
+      console.log(outJson);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
